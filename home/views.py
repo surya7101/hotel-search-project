d957c47eb0d5d40d6140142812b788c04d42030b
surya7101 <114675939+surya7101@users.noreply.github.com>
2022-09-29 20:02:55 +0530

    Add files via upload

---
+++ home/views.py
@@ -0,0 +1,40 @@
+from logging import exception
+from multiprocessing import context
+from django.shortcuts import render
+from .models import *
+from django.http import JsonResponse
+
+
+def home(request):
+    emenities = Emenities.objects.all()
+    context = {'emenities': emenities}
+    return render(request, 'home.html' , context)
+    
+def api_hotels(request):
+    hotels_objs = Hotels.objects.all()
+    price = request.GET.get('price')
+    if price:
+        hotels_objs = hotels_objs.filter(price__lte =price)
+    emenities = request.GET.get('emenities')
+    if emenities:
+        emenities = emenities.split(',')
+        em = []
+        for e in emenities:
+            try:
+                em.append(int(e))
+            except Exception as e:
+                pass
+        print(em)
+
+        hotels_objs = hotels_objs.filter(emenities__in =em).distinct()
+
+    payload = []
+    for hotel_obj in hotels_objs:
+        result ={}
+        result['hotel_name'] = hotel_obj.hotel_name
+        result['hotel_description'] = hotel_obj.hotel_description
+        result['hotel_image'] = hotel_obj.hotel_image
+        result['hotel_price'] = hotel_obj.price
+        payload.append(result)
+    return JsonResponse(payload , safe=False)
+    
